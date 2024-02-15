0298f68df0a34f2b50a2ef3f40f340a42ba4a74a
vnaidu563 <vnaidu563@gmail.com>
2024-02-15 15:54:51 -0500

    changes made in mai file

--- main1.tf
+++ main1.tf
@@ -6,7 +6,7 @@ provider "aws" {
 }
 
 # Upload the file to the existing S3 bucket
-resource "aws_s3_bucket_object" "my_object" {
+resource "aws_s3_bucket_object" "mys3" {
   bucket = "vinay-s3-bucket"  # Use the existing bucket name
   key    = "VINAY DL.jpeg"     # Name of the file in S3 bucket
   source = "C:\\Users\\vnaid\\OneDrive\\Desktop\\VINAY DL.jpeg"  # Path to the local file
