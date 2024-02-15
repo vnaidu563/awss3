cbb8cfcc106c38019a3121362ed5c9c35b712ebb
vnaidu563 <vnaidu563@gmail.com>
2024-02-15 15:51:00 -0500

    created new main1 file

---
+++ main1.tf
@@ -0,0 +1,13 @@
+# upload_file.tf
+
+# Define the provider
+provider "aws" {
+  region = "us-east-1"  # Specify the AWS region
+}
+
+# Upload the file to the existing S3 bucket
+resource "aws_s3_bucket_object" "my_object" {
+  bucket = "vinay-s3-bucket"  # Use the existing bucket name
+  key    = "VINAY DL.jpeg"     # Name of the file in S3 bucket
+  source = "C:\\Users\\vnaid\\OneDrive\\Desktop\\VINAY DL.jpeg"  # Path to the local file
+}
